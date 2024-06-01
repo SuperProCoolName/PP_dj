6e7849fafc41374b428f71e96629cd5e27c3d292
1yuken <anilf.business@gmail.com>
2024-06-01 23:52:48 +0300

    new

--- search/forms.py
+++ search/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ModelForm, TextInput, Textarea, NumberInput, FileInput
 from .models import Ad
 
 
@@ -6,3 +7,15 @@ class CreationForm(forms.ModelForm):
     class Meta:
         model = Ad
         fields = ['title', 'description', 'price', 'image']
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control__title', 'placeholder': 'Введите заголовок'}),
+            'description': Textarea(attrs={'class': 'form-control__desc', 'rows': 3, 'placeholder': 'Введите описание'}),
+            'price': NumberInput(attrs={'class': 'form-control__price', 'placeholder': 'Введите цену'}),
+            'image': FileInput(attrs={'class': 'form-control__file'}),
+        }
+    def as_p(self):
+        """Возвращает поля формы, рендеринг без названий полей"""
+        html = ''
+        for field in self.visible_fields():
+            html += f'<p>{field}</p>'
+        return html
