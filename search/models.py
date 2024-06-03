f08a1916b799a70b8079a1d827404c2638754197
SuperProCoolName <minzatov.2004@mail.ru>
2024-06-03 18:20:35 +0300

    Теперь изображение удаляется после удаления объявления

--- search/models.py
+++ search/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
+import os
 
 
 # Create your models here.
@@ -18,3 +21,26 @@ class Ad(models.Model):
 
     def __str__(self):
         return self.title
+
+
+@receiver(post_delete, sender=Ad)
+def delete_image(sender, instance, **kwargs):
+    if instance.image:
+        # Save the image file path
+        image_path = instance.image.path
+
+        # Disconnect the delete_image function from the post_delete signal
+        post_delete.disconnect(delete_image, sender=Ad)
+
+        # Delete the ad
+        instance.delete()
+
+        # Reconnect the delete_image function to the post_delete signal
+        post_delete.connect(delete_image, sender=Ad)
+
+        # Delete the image file
+        if os.path.isfile(image_path):
+            os.remove(image_path)
+
+
+post_delete.connect(delete_image, sender=Ad)
